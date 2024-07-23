a171c0cc5c2c9d86b439a5ae7b03200378316291
Ramiz Hida <ramizhida00@gmail.com>
2024-07-23 14:06:43 +0200

    Add final images and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import { Card } from './components/Card';
-import { key } from '../secret';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import React from 'react';
-import luffy from './luffy.png';
-import zoro from './zoro.png';
-import sanji from './sanji.png';
+import luffy from './images/luffy.png';
+import zoro from './images/zoro.png';
+import sanji from './images/sanji.png';
+import usopp from './images/usopp.png';
+import nami from './images/nami.png';
+import robin from './images/robin.png';
+import chopper from './images/chopper.png';
+import franky from './images/franky.png';
+import jinbe from './images/jinbe.png';
+import brook from './images/brook.png';
+import './styles/styles.css';
 
 function App() {
-  const [url, setUrl] = useState('');
   const [images, setImages] = useState([
     { imgUrl: luffy, id: '1' },
     { imgUrl: zoro, id: '2' },
-    { imgUrl: sanji, id: '4' },
+    { imgUrl: sanji, id: '3' },
+    { imgUrl: usopp, id: '4' },
+    { imgUrl: nami, id: '5' },
+    { imgUrl: robin, id: '6' },
+    { imgUrl: brook, id: '7' },
+    { imgUrl: franky, id: '8' },
+    { imgUrl: jinbe, id: '9' },
+    { imgUrl: chopper, id: '10' },
     ,
   ]);
+
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [currentCard, setCurrentCard] = useState('');
   const [gameOver, setGameOver] = useState(false);
-
-  // get image
-  // useEffect(() => {
-  //   fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${key}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setUrl(data.data[2].images.downsized.url);
-  //     });
-  // }, []);
 
   // Randomize images
   function shuffleArray(imgArr) {
@@ -52,40 +56,56 @@ function App() {
       }
     }
   }
+
   if (gameOver) {
     return (
-      <>
+      <div className="game-over">
         <h2>Game Over </h2>
-        <p>Best Score: {highScore}</p>
+        <p>
+          Best Score: <span> {highScore}</span>
+        </p>
         <button onClick={() => setGameOver(false)}>Play again</button>
-      </>
+      </div>
     );
   }
 
   return (
     <>
       <h1>Memory Card</h1>
-      <p>Dont pick the same card twice!</p>
-      {images.map((img) => {
-        {
-          if (img === undefined) return;
-        }
-        return (
-          <div
-            key={img.id}
-            className="gif-container"
-            onClick={(e) => {
-              setCurrentCard(e.target);
-              cardCheck(e.target);
-              setImages([...shuffleArray(images)]);
-            }}
-          >
-            <img src={img.imgUrl} className="gif-card" />
-          </div>
-        );
-      })}
-      <h2>Score: {score}</h2>
-      <h2>Highscore: {highScore}</h2>
+
+      <div className="heading-container">
+        <div>
+          <p id="hint">Dont pick the same card twice!</p>
+        </div>
+        <div>
+          <p>
+            Score: <span>{score}</span>
+          </p>
+          <p>
+            Highscore: <span> {highScore}</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="gif-container">
+        {images.map((img) => {
+          {
+            if (img === undefined) return;
+          }
+          return (
+            <div
+              key={img.id}
+              onClick={(e) => {
+                setCurrentCard(e.target);
+                cardCheck(e.target);
+                setImages([...shuffleArray(images)]);
+              }}
+            >
+              <img src={img.imgUrl} className="gif-card" />
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
